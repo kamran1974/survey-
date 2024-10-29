92fa2b932d539e5f3e128c902c0981784b955218
Apple <apple@Apples-MacBook-Air.local>
2024-10-29 23:06:43 +0330

    time added to date

--- restaurent/models.py
+++ restaurent/models.py
@@ -3,13 +3,13 @@ from django.db import models
 class Client(models.Model):
     name = models.CharField(max_length=255)
     phonenumber = models.CharField(max_length=11)
-    createddate = models.DateField(auto_now_add=True)
+    createddate = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 qualitychoices =(
-    (1,'good'),
+    (1, 'good'),
     (2,'moderate'),
     (3,'poor'),
     (4,'terrible')
@@ -21,7 +21,7 @@ class Opinion(models.Model):
     question1 = models.IntegerField(choices=qualitychoices)#کیفیت غذا
     question2 = models.IntegerField(choices=qualitychoices)#کیفیت پذیرایی
     idea = models.TextField()
-    submitted_date = models.DateField(auto_now_add=True)
+    submitted_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.client}{self.get_question1_display()}{self.question2}'

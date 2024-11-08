e706b4e57d89d17ca2f2a8bcd44c521b09a6e846
Apple <apple@Apples-MacBook-Air.local>
2024-11-08 14:53:47 +0330

    new data file questions updated base on fron end

--- restaurent/models.py
+++ restaurent/models.py
@@ -20,11 +20,19 @@ class Opinion(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     question1 = models.IntegerField(choices=qualitychoices)#کیفیت غذا
     question2 = models.IntegerField(choices=qualitychoices)#کیفیت پذیرایی
+    question3 = models.IntegerField(choices=qualitychoices)# آیس کارامل
+    question4 = models.IntegerField(choices=qualitychoices)#چیز کیک 
+    question5 = models.IntegerField(choices=qualitychoices)#شیک 
+    question6 = models.BooleanField(blank= True)#اقلام میز 
+    question7 = models.BooleanField(blank= True)#اقلام میز 
+    question8 = models.BooleanField(blank= True)#اقلام میز 
+    question9 = models.BooleanField(blank= True)#اقلام میز 
+    question10 = models.BooleanField(blank= True)#اقلام میز  
     idea = models.TextField()
     submitted_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.client}{self.get_question1_display()}{self.question2}'
+        return f'{self.client}{self.get_question1_display()}{self.question2}{self.question3}{self.question4}{self.question5}{self.question6}{self.question7}{self.question8}{self.question9}{self.question10}{self.idea}'
 
 
 
